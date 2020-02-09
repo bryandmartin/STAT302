4ac34049cc56a2728b874fbb497e6418f5911d29
Bryan Martin <bmartin6@uw.edu>
2020-02-08 19:48:04 -0800

    Separate house and senate votes

--- scripts/get_Project2_data.R
+++ scripts/get_Project2_data.R
@@ -3,55 +3,105 @@ library(tidyverse)
 
 # Member Ideology
 idea_116 <- read_csv("../Data/HS116_members.csv") %>%
-  filter(chamber %in% c("House", "Senate")) %>%
+  filter(chamber == "House") %>%
   mutate(party_code = recode(party_code, 
                              `100` = "D", `200` = "R", `328` = "I")) %>%
-  select(congress, chamber, icpsr, state_abbrev, party_code, bioname, born)
+  select(icpsr, state_abbrev, party_code, bioname, born)
 
 # Members' Votes
 votes_116 <- read_csv("../Data/HS116_votes.csv") %>% 
+  filter(chamber == "House") %>%
   mutate(vote = recode(cast_code,
                        `1` = 1, `6` = -1, `7` = 0, `9` = 0)) %>%
-  select(congress, chamber, icpsr, vote, rollnumber)
+  select(icpsr, vote, rollnumber)
 
 # Join and pivot Data
-congress_116 <- left_join(votes_116, idea_116, 
-                          by = c("icpsr", "congress", "chamber")) %>%
-  pivot_wider(id_cols = c(congress, icpsr, chamber, state_abbrev,
-                          party_code, bioname, born),
+house_116 <- left_join(votes_116, idea_116, 
+                          by = "icpsr") %>%
+  pivot_wider(id_cols = c(icpsr, state_abbrev, party_code, bioname, born),
               names_from = rollnumber,
-              names_prefix = "vote",
+              names_prefix = "rc",
+              values_from = vote) %>%
+  select(-icpsr)
+
+# Member Ideology
+idea_116 <- read_csv("../Data/HS116_members.csv") %>%
+  filter(chamber == "Senate") %>%
+  mutate(party_code = recode(party_code, 
+                             `100` = "D", `200` = "R", `328` = "I")) %>%
+  select(icpsr, state_abbrev, party_code, bioname, born)
+
+# Members' Votes
+votes_116 <- read_csv("../Data/HS116_votes.csv") %>% 
+  filter(chamber == "Senate") %>%
+  mutate(vote = recode(cast_code,
+                       `1` = 1, `6` = -1, `7` = 0, `9` = 0)) %>%
+  select(icpsr, vote, rollnumber)
+
+# Join and pivot Data
+senate_116 <- left_join(votes_116, idea_116, 
+                       by = "icpsr") %>%
+  pivot_wider(id_cols = c(icpsr, state_abbrev, party_code, bioname, born),
+              names_from = rollnumber,
+              names_prefix = "rc",
               values_from = vote) %>%
   select(-icpsr)
 
 # Member Ideology
 idea_90 <- read_csv("../Data/HS090_members.csv") %>%
-  filter(chamber %in% c("House", "Senate")) %>%
+  filter(chamber == "House") %>%
   mutate(party_code = recode(party_code, 
                              `100` = "D", `200` = "R", `328` = "I")) %>%
-  select(congress, chamber, icpsr, state_abbrev, party_code, bioname, born)
+  select(icpsr, state_abbrev, party_code, bioname, born)
 
 # Members' Votes
 votes_90 <- read_csv("../Data/HS090_votes.csv") %>% 
+  filter(chamber == "House") %>%
   mutate(vote = recode(cast_code,
                        `1` = 1, `2` = 1, `3` = 1,
                        `4` = -1, `5` = -1, `6` = -1,
                        `7` = 0, `8` = 0, `9` = 0)) %>%
-  select(congress, chamber, icpsr, vote, rollnumber)
+  select(icpsr, vote, rollnumber)
 
 # Join and pivot Data
-congress_90 <- left_join(votes_90, idea_90, 
-                         by = c("icpsr", "congress", "chamber")) %>%
-  pivot_wider(id_cols = c(congress, icpsr, chamber, state_abbrev,
-                          party_code, bioname, born),
+house_90 <- left_join(votes_90, idea_90, 
+                         by = "icpsr") %>%
+  pivot_wider(id_cols = c(icpsr, state_abbrev, party_code, bioname, born),
               names_from = rollnumber,
-              names_prefix = "vote",
+              names_prefix = "rc",
+              values_from = vote) %>%
+  select(-icpsr)
+
+# Member Ideology
+idea_90 <- read_csv("../Data/HS090_members.csv") %>%
+  filter(chamber == "Senate") %>%
+  mutate(party_code = recode(party_code, 
+                             `100` = "D", `200` = "R", `328` = "I")) %>%
+  select(icpsr, state_abbrev, party_code, bioname, born)
+
+# Members' Votes
+votes_90 <- read_csv("../Data/HS090_votes.csv") %>% 
+  filter(chamber == "Senate") %>%
+  mutate(vote = recode(cast_code,
+                       `1` = 1, `2` = 1, `3` = 1,
+                       `4` = -1, `5` = -1, `6` = -1,
+                       `7` = 0, `8` = 0, `9` = 0)) %>%
+  select(icpsr, vote, rollnumber)
+
+# Join and pivot Data
+senate_90 <- left_join(votes_90, idea_90, 
+                      by = "icpsr") %>%
+  pivot_wider(id_cols = c(icpsr, state_abbrev, party_code, bioname, born),
+              names_from = rollnumber,
+              names_prefix = "rc",
               values_from = vote) %>%
   select(-icpsr)
 
 
-write_csv(congress_116, path = "../Data/congress_116_raw.csv")
-write_csv(congress_90, path = "../Data/congress_90_raw.csv")
+write_csv(house_116, path = "../Data/house_116_raw.csv")
+write_csv(house_90, path = "../Data/house_90_raw.csv")
+write_csv(senate_116, path = "../Data/senate_116_raw.csv")
+write_csv(senate_90, path = "../Data/senate_90_raw.csv")
 
 
 
